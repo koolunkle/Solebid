cf50da7416b15fb7cb1c0affcc0071b844af9550
porcupine7021 <lovewl1021@naver.com>
2025-07-31 19:14:59 +0900

    feat: payment entity 생성

--- backend/demo/src/main/java/com/sesac/solbid/domain/Payments.java
+++ backend/demo/src/main/java/com/sesac/solbid/domain/Payments.java
@@ -1,6 +1,4 @@
-package com.sesac.solbid.domain;
-
-import com.sesac.solbid.domain.baseentity.BaseEntity;
+`package com.sesac.solbid.domain;import com.sesac.solbid.domain.baseentity.BaseEntity;
 import com.sesac.solbid.domain.enums.PaymentStatus;
 import jakarta.persistence.*;
 import org.hibernate.annotations.ColumnDefault;
@@ -8,31 +6,29 @@ import org.hibernate.annotations.ColumnDefault;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="payments")
+@Table(name = "payments")
 public class Payments extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id", length = 50, nullable = false)
     private Long paymentId;
-
-    private Long userId;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(name = "amount", nullable = false)
     private int amount;
-
+    @Column(name = "payment_method", length = 20, nullable = false)
     private String paymentMethod;
-
+    @Column(name = "transaction_id", length = 100)
+    private String transactionId;
     @Enumerated(EnumType.STRING)
     private PaymentStatus paymentStatus;
-
     @ColumnDefault("0")
+    @Column(name = "converted_point", nullable = false)
     private int convertedPoint;
-
+    @Column(name = "is_charged", nullable = false)
     @ColumnDefault("false")
     private Boolean isCharged;
-
     private LocalDateTime requestedAt;
-
     private LocalDateTime confirmedAt;
-
-
-}
+}`
--- backend/demo/src/main/java/com/sesac/solbid/domain/PointTransaction.java
+++ backend/demo/src/main/java/com/sesac/solbid/domain/PointTransaction.java
@@ -1,36 +1,30 @@
-package com.sesac.solbid.domain;
-
-
-import com.sesac.solbid.domain.enums.TransEnum;
+`package com.sesac.solbid.domain;import com.sesac.solbid.domain.enums.TransEnum;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="point_transaction")
+@Table(name = "point_transaction")
 public class PointTransaction {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
     private Long transactionId;
-
-    private Long userId;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
     @Enumerated(EnumType.STRING)
     private TransEnum transEnum;
-
     @Column(unique = true, nullable = false)
     private BigDecimal balanceAfter;
-
+    @Column(length = 225)
     private String description;
-
-    private String paymentMethod;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_id")
+    private Payments payment_id;
     @Column(unique = true, nullable = false)
     private LocalDateTime createdAt;
-
-
-
-
-}
+    @Column(name = "point", nullable = false)
+    private int point;
+}`
